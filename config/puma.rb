6648a47e3807a73507752cd83d8589f8eef6c9e2
naveen <prnaveen2112@gmail.com>
2023-04-11 23:50:54 +0530

    second commit

--- config/puma.rb
+++ config/puma.rb
@@ -41,3 +41,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
